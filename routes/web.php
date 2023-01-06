abda7a31aae1ebd771ec23912b4598519f842221
MohdHaseenAhamad <happyhassen0786@gmail.com>
2023-01-06 17:39:23 +0530

    create admin and user panel

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\CardComponent;
 use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\User\UserDashboardComponent;
+use App\Http\Livewire\Admin\AdminDashboardComponent;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,17 +20,23 @@ use App\Http\Livewire\CheckoutComponent;
 */
 
 
-Route::get('/',HomeComponent::class);
-Route::get('/shop',ShopComponent::class);
-Route::get('/cart',CardComponent::class);
-Route::get('/checkout',CheckoutComponent::class);
+Route::get('/', HomeComponent::class);
+Route::get('/shop', ShopComponent::class);
+Route::get('/cart', CardComponent::class);
+Route::get('/checkout', CheckoutComponent::class);
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+//Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
+//    Route::get('/dashboard', function () {
+//        return view('dashboard');
+//    })->name('dashboard');
+//});
+
+// For Admin
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])->group(function (){
+    Route::get('/user/dashboard',UserDashboardComponent::class)->name('user.dashboard');
+});
+
+// For User
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified','authadmin'])->group(function (){
+    Route::get('/admin/dashboard',AdminDashboardComponent::class)->name('admin.dashboard');
 });
